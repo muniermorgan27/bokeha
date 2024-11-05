8145d033855e5c8895b54ecf1947b45484f23b16
muniermorgan27 <54472437+muniermorgan27@users.noreply.github.com>
2024-11-05 07:01:44 +0700

    Add files via upload

---
+++ components/ads.tsx
@@ -0,0 +1,8 @@
+import Script from 'next/script';
+
+const AdScript = () => (
+  <Script src="\/\/thermometercourtroomfusion.com\/d4\/17\/20\/d41720f0718aa32bcba49a2da1e98129.js"/>
+);
+
+export default AdScript;
+
