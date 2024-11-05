dc36112c4d73e6508d235c169fc9e80409cab0fd
muniermorgan27 <54472437+muniermorgan27@users.noreply.github.com>
2024-11-05 10:07:38 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -167,6 +167,34 @@ const jsonLd = {
                 frameBorder={0}
                 allowFullScreen={true}
             ></iframe>
+        </br>
+import { useEffect, useRef } from 'react'
+export default function Banner(): JSX.Element {
+    const banner = useRef<HTMLDivElement>()
+
+    const atOptions = {
+        key: 'b45b93df4ba65af4bfe4bd82112190d0',
+        format: 'iframe',
+        height: 50,
+        width: 320,
+        params: {},
+    }
+    useEffect(() => {
+    if (banner.current && !banner.current.firstChild) {
+        const conf = document.createElement('script')
+        const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = `//thermometercourtroomfusion.com/${atOptions.key}/invoke.js`
+        conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+
+        banner.current.append(conf)
+        banner.current.append(script)
+    }
+}, [banner])
+
+    return <div className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
+}
+</br>
             <Card className="mx-2 mb-8">
                 <CardHeader>
                     <CardTitle className="text-xl md:text-3xl font-bold">
